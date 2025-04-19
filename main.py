46abb8a6d09718788fb0122758be3074a8a00f55
CyberNetGnome <Nobles4n@gmail.com>
2025-04-19 21:28:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
-print("hello bienvenue sur le jeu juste prix")
+import random
+
+print("Bienvenue dans le jeu du Juste Prix !")
+aide = input("Si vous ne connaissez pas les règles, tapez 'help' : ").lower()
+
+if aide == 'help':
+    print("Le jeu est simple : vous devez trouver un nombre aléatoire entre 0 et 100.")
+else:
+    print("Donc vous connaissez déjà les règles, bien, continuons !")
+
+jouer = input("Voulez-vous jouer ? (oui/non) : ").lower()
+
+if jouer != 'oui':
+    print("Dommage, à la prochaine !")
+    exit()
+
+print("Allons-y ! Essayez de deviner le nombre mystère.")
+
+nombre_mystere = random.randint(0, 100)
+nombre = -1  # Valeur par défaut différente du nombre_mystère
+
+while nombre != nombre_mystere:
+    try:
+        nombre = int(input("Entrez un nombre entre 0 et 100 : "))
+        if nombre < 0 or nombre > 100:
+            print("Le nombre doit être entre 0 et 100.")
+        elif nombre > nombre_mystere:
+            print("C'est moins !")
+        elif nombre < nombre_mystere:
+            print("C'est plus !")
+        else:
+            print("Bravo, vous avez trouvé le juste prix !")
+    except ValueError:
+        print("Veuillez entrer un nombre valide.")
